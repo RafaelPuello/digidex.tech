2bd08e4f235c8dd6895301fdd304a234ab892b23
Raphael <rafael.e.puello@gmail.com>
2024-09-15 08:56:24 -0600

    Add new templates for account management and authentication

--- backend/biodiversity/wagtail_hooks.py
+++ backend/biodiversity/wagtail_hooks.py
@@ -1,5 +1,23 @@
 from wagtail import hooks
+from wagtail.admin.panels import TitleFieldPanel, FieldPanel, MultiFieldPanel
+from wagtail.snippets.models import register_snippet
+from wagtail.snippets.views.snippets import SnippetViewSet
+
+from .models import Plant
 
 @hooks.register("register_icons")
 def register_icons(icons):
     return icons + ['biodiversity/icons/plant.svg']
+
+
+class PlantViewSet(SnippetViewSet):
+    model = Plant
+    icon = "plant"
+    add_to_admin_menu = True
+
+    panels = [
+        TitleFieldPanel("name"),
+        FieldPanel("description"),
+    ]
+
+register_snippet(PlantViewSet)
