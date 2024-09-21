95581764385e5bf65ad56b4420e5a11eb295ed5a
Raphael <rafael.e.puello@gmail.com>
2024-09-20 21:49:41 -0600

    Refactor: Remove unused code and update project structure in accounts adapter

--- backend/ntags/apps.py
+++ backend/ntags/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 
 class NtagsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
     name = 'ntags'
+    verbose_name = _("Taggit")
+    default_auto_field = 'django.db.models.BigAutoField'
