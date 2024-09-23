a867aa1ecc692f73dff41304017e30d3cb861118
Raphael <rafael.e.puello@gmail.com>
2024-09-23 10:10:55 -0600

    Refactor: Update NFCTag model and related code

--- backend/ntags/tests.py
+++ backend/ntags/tests.py
@@ -1,15 +1,20 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+from .models import NFCTag
+
 User = get_user_model()
 
 
 class NFCTagDesignModelTest(TestCase):
-
     def setUp(self):
-        # Set up initial data for tests
+        """
+        Set up the test environment.
+        """
         self.user = User.objects.create(username="testuser")
 
     def test_str_representation(self):
-        """Test the string representation of the model."""
+        """
+        Test the string representation of the model.
+        """
         self.assertEqual(str(self.ntag_design), "Test Design")
--- backend/ntags/urls.py
+++ backend/ntags/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from . import views
 
+app_name = "ntags"
 urlpatterns = [
     path("", views.link, name="link"),
 ]
