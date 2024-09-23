fbd5aa9192cadca0d87e97b4a001acdbca241581
Raphael <rafael.e.puello@gmail.com>
2024-09-23 11:01:36 -0600

    Refactor: Remove unused testing configuration files and code

--- backend/ntags/tests.py
+++ backend/ntags/tests.py
@@ -1,20 +1,12 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
-from .models import NFCTag
-
 User = get_user_model()
 
 
-class NFCTagDesignModelTest(TestCase):
+class NFCTagModelTest(TestCase):
     def setUp(self):
         """
         Set up the test environment.
         """
         self.user = User.objects.create(username="testuser")
-
-    def test_str_representation(self):
-        """
-        Test the string representation of the model.
-        """
-        self.assertEqual(str(self.ntag_design), "Test Design")
