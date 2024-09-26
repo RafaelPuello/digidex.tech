24040808b611bc22ff06a7cfb5c169dfdd5d4f14
Raphael <rafael.e.puello@gmail.com>
2024-09-26 11:10:52 -0600

    Add initial files for the contact app

---
+++ backend/contact/models.py
@@ -0,0 +1,42 @@
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import (
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel
+)
+from wagtail.fields import RichTextField
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
+
+
+class ContactFormField(AbstractFormField):
+    page = ParentalKey(
+        'FormPage',
+        on_delete=models.CASCADE,
+        related_name='form_fields'
+    )
+
+
+class ContactFormPage(AbstractEmailForm):
+    intro = RichTextField(
+        blank=True
+    )
+    thank_you_text = RichTextField(
+        blank=True
+    )
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
