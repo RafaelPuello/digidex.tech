9ee99ac7068e0770c05b10107ef4a8c269d30817
Raphael <rafael.e.puello@gmail.com>
2024-10-06 17:14:26 -0600

    Refactor CI workflow to remove Pytest step

--- backend/digidex/settings/testing.py
+++ backend/digidex/settings/testing.py
@@ -1,4 +1,8 @@
 from .base import *  # noqa
+import os  # noqa: F405
+from django.core.management.utils import get_random_secret_key
+
+SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
 DEBUG = False
 
--- backend/ntags/utils.py
+++
@@ -1,9 +0,0 @@
-NTAG_USERS_PERMISSIONS = [
-    'change_nfctag', 'view_nfctag',
-    'view_nfctageeprom', 'view_nfctagscan'
-]
-
-
-def assign_ntag_user_permissions(group):
-    from base.utils import assign_group_permissions
-    return assign_group_permissions(group, NTAG_USERS_PERMISSIONS)
