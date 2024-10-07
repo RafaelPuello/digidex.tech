8a0d345cbdbf8c48acd57aeb693e4aa4cf3a9879
Raphael <rafael.e.puello@gmail.com>
2024-10-07 15:15:03 -0600

    Refactor serializers and inventory utils to pass flake8

--- backend/inventory/utils.py
+++ backend/inventory/utils.py
@@ -7,4 +7,4 @@ from .models import InventoryIndexCollection
 def setup_user_inventory(user):
     user.add_to_group("Trainers")
     user_collection = InventoryIndexCollection.get_for_user(user)
-    user_inventory = user_collection.get_user_page()
+    user_collection.get_user_page()  # Make sure the user has an inventory page
