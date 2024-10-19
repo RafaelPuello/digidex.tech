a89b0b7212b4c315d510a5d6b60708f7123ddd3a
Raphael <rafael.e.puello@gmail.com>
2024-10-18 20:13:22 -0600

    Refactor BaseCardBlock to include an optional image field

--- backend/botany/wagtail_hooks.py
+++ backend/botany/wagtail_hooks.py
@@ -3,6 +3,7 @@ from wagtail.snippets.models import register_snippet
 
 from .viewsets import UserPlantViewSet
 
+
 @hooks.register("register_icons")
 def register_icons(icons):
     return icons + ['botany/icons/plant.svg']
