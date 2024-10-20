bb659aa9deaba53441af4d3675e38841a14e621c
Raphael <rafael.e.puello@gmail.com>
2024-10-19 22:04:18 -0600

    Refactor botany module to include PlantSubstrate and SubstrateMix models

--- backend/botany/models.py
+++ backend/botany/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models, transaction
+from django.conf import settings
 from django.utils.text import slugify
 from django.utils.translation import gettext_lazy as _
 from modelcluster.models import ClusterableModel
@@ -14,6 +15,62 @@ from .blocks import BotanyNoteBlock
 from .forms import UserPlantForm
 
 
+class PlantSubstrate(models.Model):
+    """
+    This model represents different substrates for growing plants.
+    Examples include soil, hydroponics, coco coir, etc.
+    """
+    name = models.CharField(
+        max_length=255,
+        unique=True
+    )
+    description = models.TextField(
+        blank=True
+    )
+
+    class Meta:
+        verbose_name = _('Plant Substrate')
+        verbose_name_plural = _('Plant Substrates')
+
+    def __str__(self):
+        return self.name
+
+
+class SubstrateMix(models.Model):
+    """
+    This model represents a mixture of substrates for growing plants.
+    Examples include 50% soil, 50% perlite, etc.
+    """
+    name = models.CharField(
+        max_length=255,
+        unique=True
+    )
+    description = models.TextField(
+        blank=True
+    )
+    substrates = models.ManyToManyField(
+        PlantSubstrate,
+        related_name='mixes'
+    )
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        related_name='substrate_mixes',
+        on_delete=models.SET_NULL
+    )
+    metadata = models.JSONField(
+        blank=True,
+        null=True
+    )
+
+    class Meta:
+        verbose_name = _('Substrate Mix')
+        verbose_name_plural = _('Substrate Mixes')
+
+    def __str__(self):
+        return self.name
+
+
 class UserPlant(
     Orderable,
     ClusterableModel,
@@ -21,6 +78,17 @@ class UserPlant(
     TranslatableMixin,
     PreviewableMixin
 ):
+    uuid = models.UUIDField(
+        default=uuid.uuid4,
+        editable=False,
+        unique=True,
+        db_index=True
+    )
+    slug = models.SlugField(
+        editable=False,
+        db_index=True,
+        max_length=255
+    )
     box = models.ForeignKey(
         'inventory.InventoryFormPage',
         related_name='plants',
@@ -33,16 +101,12 @@ class UserPlant(
     description = models.TextField(
         blank=True
     )
-    uuid = models.UUIDField(
-        default=uuid.uuid4,
-        editable=False,
-        unique=True,
-        db_index=True
-    )
-    slug = models.SlugField(
-        editable=False,
-        db_index=True,
-        max_length=255
+    substrate_mix = models.ForeignKey(
+        'SubstrateMix',
+        related_name='user_plants',
+        on_delete=models.SET_NULL,
+        blank=True,
+        null=True,
     )
     notes = StreamField(
         [('note', BotanyNoteBlock())],
